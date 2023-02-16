8c74013f59a8b9ba996986990762a35e78af881a
unxed <unxed@mail.ru>
2023-02-16 23:03:56 +0100

    Corresponding changes for b1f6a97325bcde8474b750e1f29f908c5b1d44ed

--- portable/make_far2l_portable_on_ubuntu_16_04.sh
+++ portable/make_far2l_portable_on_ubuntu_16_04.sh
@@ -5,7 +5,7 @@ rm -rf far2l
 mkdir far2l
 cd far2l
 apt-get update
-apt-get install -y libspdlog-dev patchelf wget gawk m4 libx11-dev libxi-dev libxerces-c-dev libuchardet-dev libssh-dev libssl-dev libnfs-dev libneon27-dev libarchive-dev libpcre3-dev cmake g++ git
+apt-get install -y libspdlog-dev patchelf wget gawk m4 libx11-dev libxi-dev libxerces-c-dev libuchardet-dev libssh-dev libssl-dev libnfs-dev libneon27-dev libpcre3-dev cmake g++ git
 git clone https://github.com/elfmz/far2l
 cd far2l
 wget https://raw.githubusercontent.com/unxed/far2l-deb/master/portable/tty_tweaks.patch
@@ -18,14 +18,6 @@ cd install
 wget https://github.com/unxed/far2l-deb/raw/master/portable/autonomizer.sh
 chmod +x autonomizer.sh
 ./autonomizer.sh
-rm lib/libc.so.6
-rm lib/libdl.so.2
-rm lib/libgcc_s.so.1
-rm lib/libm.so.6
-rm lib/libpthread.so.0
-rm lib/libstdc++.so.6
-rm lib/libresolv.so.2
-rm lib/librt.so.1
 cd ..
 mv install far2l_portable
 git clone https://github.com/megastep/makeself.git
