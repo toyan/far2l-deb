2172b530e8062d96ced5aae057b0f944e10e4cbd
unxed <unxed@mail.ru>
2023-02-17 14:30:09 +0100

    Add files via upload

--- portable/far2l.sh
+++ portable/far2l.sh
@@ -3,5 +3,7 @@ pushd "$(pwd)"
 cd "$(dirname "$0")"
 chmod +x lib/ld-linux*
 chmod +x far2l
+find . -type f -iname "*far-plug*" -exec chmod +x {} \;
+find . -type f -iname "*.broker" -exec chmod +x {} \;
 ./far2l
 popd
