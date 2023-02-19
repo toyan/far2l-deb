c25eeb32047b55e519fd3896b0e6af2828a1d727
unxed <unxed@mail.ru>
2023-02-19 01:16:30 +0100

    Update far2l.sh

--- portable/far2l.sh
+++ portable/far2l.sh
@@ -3,6 +3,8 @@ pushd "$(pwd)"
 cd "$(dirname "$0")"
 chmod +x lib/ld-linux*
 chmod +x far2l
+chmod +x far2l_sudoapp
+chmod +x far2l_askpass
 find . -type f -iname "*far-plug*" -exec chmod +x {} \;
 find . -type f -iname "*.broker" -exec chmod +x {} \;
 ./far2l
